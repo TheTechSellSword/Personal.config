988aed8cc79e27a44018f5a8cd7c054777305bbc
thetechsellsword <thetechsellsword@gmail.com>
2024-02-28 23:39:11 -0600

    Created gRPC Go Plugin install script.

--- golang/install-gRPC-go-plugin-linux-osx.sh
+++ golang/install-gRPC-go-plugin-linux-osx.sh
@@ -2,6 +2,10 @@
 
 # VARIABLES
 brcPath="$HOME/.bashrc"
-addDirectoryToPath="export PATH=$PATH:/$HOME/.local/bin"
+addDirectoryToPath="export PATH=$PATH:$(go env GOPATH)/bin"
 
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+echo $addDirectoryToPath >> $brcPath
+
+protoc-gen-go --version
+protoc-gen-go-grpc --version
